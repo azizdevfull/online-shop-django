52691ee7636565ba9d8edc6f6ccf65811bcba5ad
azizdevfull <aziz16110904@gmail.com>
2023-02-16 13:05:28 +0500

    add category model and template

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('', views.home ),
-]
+    path('category/<slug:val>', views.CategoryView.as_view(),name="category"),
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- app/views.py
+++ app/views.py
@@ -1,5 +1,13 @@
+from django.db.models import Count
 from django.shortcuts import render
-
+from django.views import View
+from . models import Product
 # Create your views here.
 def home(request):
     return render(request, 'app/home.html')
+
+class CategoryView(View):
+    def get (self, request, val):
+        product = Product.objects.filter(category=val)
+        title = Product.objects.filter(category=val).values('title').annotate(total=Count('title'))
+        return render (request, "app/category.html", locals())

77a3c6cb935bb4d81e190a6e4629b2c61b770e93
azizdevfull <aziz16110904@gmail.com>
2023-02-16 11:55:55 +0500

    create Product model and add to Admin site

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from . models import Product
 # Register your models here.
+@admin.register(Product)
+class ProductModelAdmin(admin.ModelAdmin):
+    list_display = ['id','title','discounted_price','category','product_image']

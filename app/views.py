8d389c738fc530582cb29e935ddc48b6981114bd
azizdevfull <aziz16110904@gmail.com>
2023-02-16 13:58:39 +0500

    add category titlr template

--- app/views.py
+++ app/views.py
@@ -6,8 +6,26 @@ from . models import Product
 def home(request):
     return render(request, 'app/home.html')
 
+def about(request):
+    return render(request, 'app/about.html')
+
+def contact(request):
+    return render(request, 'app/contact.html')
+
 class CategoryView(View):
-    def get (self, request, val):
+    def get(self, request, val):
         product = Product.objects.filter(category=val)
         title = Product.objects.filter(category=val).values('title').annotate(total=Count('title'))
         return render (request, "app/category.html", locals())
+
+class CategoryTitle(View):
+    def get(self, request, val):
+        product = Product.objects.filter(title=val)
+        title = Product.objects.filter(category=product[0].category).values('title')
+        return render (request, "app/category.html", locals())
+    
+class ProductDetail(View):
+    def get(self,request, pk):
+        product = Product.objects.get(pk=pk)
+        return render(request, "app/product_detail.html", locals())
+        
